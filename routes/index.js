cf5a5abca49f9fa70688cfbcbcaab9177b7b67cc
Javier <javier.colon15@upr.edu>
2015-09-28 23:10:44 -0400

    added ferni

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/ferni', function(req, res, next) {
+  res.render('index', { title: 'ferni' });
+});
+
 module.exports = router;
