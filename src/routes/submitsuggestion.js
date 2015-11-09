752d72a3a02b17de42606aad5e80d14803667198
javier.colon15@upr.edu <javier.colon15@upr.edu>
2015-11-09 01:49:53 -0400

    fixed suggestion route

--- src/routes/submitsuggestion.js
+++ src/routes/submitsuggestion.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET About Us page. */
 router.get('/', function(req, res, next) {
-  res.render('submit/sUBMITsUGGESTIONS', { sitename: 'Submit Suggestion' });
+  res.render('submit/SubmitSuggestions', { sitename: 'Submit Suggestion' });
 });
 
 module.exports = router;
