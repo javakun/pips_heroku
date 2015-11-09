c1cac09e41eae7bc39b6c4deb43662afeab4e5ca
javier.colon15@upr.edu <javier.colon15@upr.edu>
2015-11-09 03:27:55 -0400

    Fixed the layout in the login and create account

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET About Us page. */
 router.get('/', function(req, res, next) {
-  res.render('page/LoginPage', {layout: 'Loginlayout.hbs'}, { sitename: 'Login' });
+  res.render('page/LoginPage',{ layout: 'Loginlayout', sitename: 'Login' });
 });
 
 module.exports = router;
