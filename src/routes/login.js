75d159b84e03d98cc50e9d3808c2902fbf5a702e
javier.colon15@upr.edu <javier.colon15@upr.edu>
2015-11-09 02:52:46 -0400

    try1 to remove navbar from login

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET About Us page. */
 router.get('/', function(req, res, next) {
-  res.render('page/LoginPage', { sitename: 'Login' });
+  res.render('page/LoginPage', {layout: 'Loginlayout.hbs'}, { sitename: 'Login' });
 });
 
 module.exports = router;
