506a0921358811e4e91237fc8644433ade166132
javier.colon15@upr.edu <javier.colon15@upr.edu>
2015-11-09 06:35:35 -0400

    Added routing from the login page

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var LoginTest = require('./LoginTest.json');
 
 /* GET About Us page. */
 router.get('/', function(req, res, next) {
-  res.render('page/LoginPage',{ layout: 'Loginlayout', sitename: 'Login' });
+  res.render('page/LoginPage',{ 
+    layout: 'Loginlayout', 
+    sitename: 'Login',
+    test: LoginTest
+     });
 });
 
 module.exports = router;
