0c991ecac55e280d9e5575a86fd0c8739b3eafb7
FernandoMari <fernando.mari@upr.edu>
2015-11-10 02:15:13 -0400

    Some work on json and viewtags, nothing complete.

--- src/routes/viewalltags.js
+++ src/routes/viewalltags.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var tags = require('./tags.json')
+
 
 /* GET About Us page. */
 router.get('/', function(req, res, next) {
-  res.render('view/ViewAllTags', { sitename: 'Tags' });
+  res.render('view/ViewAllTags', { sitename: 'Tags' , tagList : tags});
 });
 
 module.exports = router;
