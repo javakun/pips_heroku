4391ae060603bf11efca218c33ec2b035fb4880e
javier.colon15@upr.edu <javier.colon15@upr.edu>
2015-11-18 21:20:06 -0400

    Working copy

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var LoginTest = require('./LoginTest.json');
 
 /* GET About Us page. */
 router.get('/', function(req, res, next) {
   res.render('page/LoginPage',{ 
     layout: 'Loginlayout', 
     sitename: 'Login',
-    test: LoginTest
+   
      });
 });
 
