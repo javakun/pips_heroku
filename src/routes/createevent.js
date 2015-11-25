caa822c423e6861cd36ae21ad9c8d07d96610416
FernandoMari <fernando.mari@upr.edu>
2015-11-25 18:41:16 -0400

    DB things for CreateEvent

--- src/routes/createevent.js
+++ src/routes/createevent.js
@@ -1,6 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+//DB CLIENT
+var client = require('../../db').getClient()
+
+//Method to post event information into DB
+router.post('/createEv', function(req, res, next) {
+  var Event_Name = req.body.EName;
+  var Event_Desc = req.body.EDesc;
+  var Event_Date = req.body.EDate;
+  var Event_Loc = req.body.ELoc;
+  var Event_Admin = req.session.user.id;
+  var Event_Members = null;
+  var Event_Tags= req.body.tagsinput;
+  var row_c = client.query("SELECT COUNT(*) FROM event") + 1;
+  
+  client.query("INSERT INTO event VALUES($1, $2, $3, $4, $5, $6, $7, $8)", [row_c, Event_Name, Event_Desc, Event_Date, Event_Loc, Event_Admin, Event_Members, Event_Tags]);
+});
+
 /* GET About Us page. */
 router.get('/', function(req, res, next) {
   res.render('create/CreateEventPage.html', { sitename: 'Create Event' });
