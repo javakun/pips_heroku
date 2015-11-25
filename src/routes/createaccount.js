5a39e04dd611d45eac050dbfdf0e4a79358f04f4
FernandoMari <fernando.mari@upr.edu>
2015-11-25 01:15:04 -0400

    Started post methods for CreateProfile and SubmitResume

--- src/routes/createaccount.js
+++ src/routes/createaccount.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+//DB CLIENT
+var client = require('../../db').getClient()
+
+//Method to post account information into DB
+router.post('/', function(req, res, next) {
+  
+  
+});
+
 /* GET About Us page. */
 router.get('/', function(req, res, next) {
   res.render('create/CreateAccountPage.html', {  sitename: 'Create Account' });
--- src/routes/submitresume.js
+++ src/routes/submitresume.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+//DB CLIENT
+var client = require('../../db').getClient()
+
+//Method to post resume into db
+router.post('/submitR', function (req, res) {
+  var User_ID = req.session.user.id;
+  var Resume_Body = req.body.editor1;
+  var row_c = client.query("SELECT COUNT(*) FROM resume");
+  client.query("INSERT INTO resume VALUES($1, $2, $3)",[row_c + 1,Resume_Body,User_ID]);
+});
+
 /* GET Submit Resume page. */
 router.get('/', function(req, res, next) {
   res.render('submit/SubmitResumePage.html', { sitename: 'Submit Resume' });
