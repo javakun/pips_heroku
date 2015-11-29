5ca5996e0df3f950f00aa5ca285cd56330f6a1b1
rasevise <rafael.vissepo@upr.edu>
2015-11-29 15:35:40 -0400

    fixed some issues with event.js

--- src/routes/event.js
+++ src/routes/event.js
@@ -4,42 +4,9 @@ var client = require('../../db').getClient();
 
 /* GET About Us page. */
 router.get('/', function (req, res) {
-  // Method to query information from different tables to fill the profile.
-
-  var result = {};
-
-  client.query("SELECT * FROM event WHERE event.member_list @> '{$1}'::int[];", [req.session.user.id], selectEvent);
-  function selectEvent(err, results) {
-        result.events = results.rows.map(function (event) {
-              return {
-                id: event.event_id,
-                event_name: event.event_name,
-                event_location: event.event_loc,
-                date: event.event_date,
-                description: event.event_description,
-                admin: event.admin_id,
-                member_list: event.member_list,
-                tag_list: event.tag_list
-              }
-        });
-
-    client.query("SELECT * FROM event WHERE event.member_list @> '{$1}'::int[];",
-        [req.session.user.id],
-        displayData);
-  }
-
-  function displayData(err, results) {
-      res.render('page/EventsPage.html', {
-        //information to be used for template filling
-        sitename: 'Events',
-        events: results.events
+      res.render('page/EventPage.html', {
+        sitename: 'Events'
       })
-  }
 
 });
 module.exports = router;
-
-//<h1><%= name %></h1>
-//<p><%= description %></p>
-// <label>Event Date:</label>
-//<label><%= date %></label>
