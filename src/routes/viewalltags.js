af617aff6c4857d987c730483ec903b766c57565
FernandoMari <fernando.mari@upr.edu>
2015-11-30 04:24:11 -0400

    WIP: ViewAllTags and ViewTagged ViewAllTags successfully displays tags and transfers to ViewTagged, however VT doesnt display projects, groups and events correctly yet.

--- src/routes/viewalltags.js
+++ src/routes/viewalltags.js
@@ -1,42 +1,36 @@
 var express = require('express');
 var router = express.Router();
-var client = require('../../db').getClient()
+var client = require('../../db').getClient();
 
 //Method to fill the page with tag info from DB
 router.get('/', function (req, res) {
   var result = {};
-
   client.query("SELECT * FROM tags", selectTags)
-
-  function selectTags(err, result) {
-    result.tags = result.rows.map(function (tag) {
-      return { tag: tag.tag_name }
+ 
+  function selectTags(err, results) {
+    result.tags = results.rows.map(function (tag) {
+      return { 
+        tag_id: tag.tag_id,
+        tag_name: tag.tag_name }       
+    });
+    
+    res.render('view/ViewAllTags.html', {
+      sitename: 'Tags',
+      tags: result.tags,
     });
   }
-
-  if (result.rows[0]) {
-    if (result.tags.length() == 0) {
-      result.posts = null;
-      res.render('view/ViewAllTags.html', {
-        sitename: 'Tags',
-        tags: result.tags
-      });
-    }
-    else {
-      res.render('view/ViewAllTags.html', {
-        sitename: 'Tags',
-        tags: result.tags
-      });
-    }
-  }
-
-
 });
 
-/* GET ViewAllTags page. */
-router.get('/', function (req, res, next) {
-  res.render('view/ViewAllTags.html', {
-    sitename: 'Tags',
+//Method to give ViewTagged the info of the tag we clicked
+router.get('/viewTagged', function (req, res, next) { 
+  var url = req.url;
+  var lastWord = url.split("/viewTagged?tagButton=").join('');
+  var tag_id = client.query("SELECT tag_id FROM tags WHERE tags.tag_name = $1",[lastWord]);
+  
+  res.render('view/ViewTagged.html', {
+    sitename: 'View Tagged',
+    tag_name: lastWord,
+    tag_id: tag_id
   });
 });
 
--- src/routes/viewtagged.js
+++ src/routes/viewtagged.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var client = require('../../db').getClient();
 
 /* GET About Us page. */
-router.get('/', function(req, res, next) {
-  res.render('view/ViewTagged.html', { sitename: 'About PIPS' });
+router.get('/', function (req, res, next) {
+	res.render('view/ViewTagged.html', {
+		sitename: 'View Tagged',
+    });
 });
 
 module.exports = router;
