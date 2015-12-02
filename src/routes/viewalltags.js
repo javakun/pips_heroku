62661bff7a8d5221e2f2f53015b61e98c8bda44f
javier.colon15@upr.edu <javier.colon15@upr.edu>
2015-12-01 20:09:01 -0400

    no message

--- src/routes/viewalltags.js
+++ src/routes/viewalltags.js
@@ -5,22 +5,52 @@ var client = require('../../db').getClient();
 //Method to fill the page with tag info from DB
 router.get('/', function (req, res) {
   var result = {};
+  
+  client.query("SELECT * FROM notifications WHERE notifications.user_id = $1",
+    [req.session.user.id],
+    selectNotifications);
+
+
+  function selectNotifications(err, results) {
+    result.notifications = results.rows.map(function (notification) {
+      return {
+        id: notification.noti_id,
+        description: notification.noti_description,
+        title: notification.noti_title,
+        link: notification.noti_link
+      }
+    });
+    
   client.query("SELECT * FROM tags", selectTags);
+  }
+
  
   function selectTags(err, results) {
     result.tags = results.rows.map(function (tag) {
       return { 
         tag_id: tag.tag_id,
-        tag_name: tag.tag_name }       
+        tag_name: tag.tag_name }   
+        
+        client.query("SELECT * FROM profile WHERE profile.profile_id = $1",
+      [req.session.user.id], displayData);    
     });
-    
+  }
+  function displayData(err, results) {    
     res.render('view/ViewAllTags.html', {
-      sitename: 'Tags',
+      
       tags: result.tags,
-      //profile_name: username
+      pagename: 'PIPS - Submit Suggestion',
+      profile_name: results.rows[0].profile_name,
+      notification: result.notifications,
+      admin_name: req.session.user.name,
+      user_email: req.session.user.user_email
+  
     });
   }
 });
+ 
+
+  
 
 //Method to give ViewTagged the info of the tag we clicked
 router.get('/viewTagged', function (req, res, next) { 
