547c35de93d10260f7296f2bb91b10a5730176c5
javier.colon15@upr.edu <javier.colon15@upr.edu>
2015-12-01 22:03:21 -0400

    work view all tagged and view tagged .js  only need to work on the html

--- src/routes/viewalltags.js
+++ src/routes/viewalltags.js
@@ -5,7 +5,7 @@ var client = require('../../db').getClient();
 //Method to fill the page with tag info from DB
 router.get('/', function (req, res) {
   var result = {};
-  
+
   client.query("SELECT * FROM notifications WHERE notifications.user_id = $1",
     [req.session.user.id],
     selectNotifications);
@@ -20,118 +20,35 @@ router.get('/', function (req, res) {
         link: notification.noti_link
       }
     });
-    
-  client.query("SELECT * FROM tags", selectTags);
+
+    client.query("SELECT * FROM tags", selectTags);
   }
 
- 
+
   function selectTags(err, results) {
     result.tags = results.rows.map(function (tag) {
-      return { 
+      return {
         tag_id: tag.tag_id,
         tag_name: tag.tag_name
-         }   
-        }); 
-        client.query("SELECT * FROM profile WHERE profile.profile_id = $1",
-      [req.session.user.id], displayData);    
-   
+      }
+    });
+    client.query("SELECT * FROM profile WHERE profile.profile_id = $1",
+      [req.session.user.id], displayData);
+
   }
-  function displayData(err, results) {    
+  function displayData(err, results) {
     res.render('view/ViewAllTags.html', {
-      
+
       tags: result.tags,
-      pagename: 'PIPS - Submit Suggestion',
+      pagename: 'PIPS - View All Tags',
       profile_name: results.rows[0].profile_name,
       notification: result.notifications,
       admin_name: req.session.user.name,
       user_email: req.session.user.user_email
-  
+
     });
   }
 });
  
-
-  
-
-//Method to give ViewTagged the info of the tag we clicked
-router.get('/viewTagged', function (req, res, next) { 
-  //var url = req.url;
-  //console.log(url)
-  var result = {}
-  var lastWord = {}
-   lastWord = req.query.tagButton; //url.split("/viewTagged?tagButton=").join('');
-  console.log(lastWord);
- 
-  client.query("SELECT project_id, project_name FROM project WHERE project.tag_id = $1",[lastWord]);
-  
-  function selectProjects(err, results) {
-    result.projects = results.rows.map(function(project) {
-      return{
-        p_id: project.project_id,
-        p_name: project.project_name
-      }
-    });
-    client.query("SELECT group_id, group_name FROM groups WHERE groups.tag_id = $1", [lastWord], selectGroups);
-  }
-  function selectGroups(err, results) {
-    result.group = results.rows.map(function(group) {
-      return{
-        g_id: group.group_id,
-        g_name: group.group_name
-      }
-    });
-    client.query("SELECT event_id, event_id FROM event WHERE event.tag_id = $1", [lastWord], selectEvent); 
-  }
-  function selectEvent(err, results) {
-    result.event = results.rows.map(function(event) {
-      return{
-        e_id: event.event_id,
-        e_name: event.event_name
-      }
-    });
-  client.query("SELECT * FROM notifications WHERE notifications.user_id = $1",
-    [req.session.user.id],
-    selectNotifications);
-  }
-
-  function selectNotifications(err, results) {
-    result.notifications = results.rows.map(function (notification) {
-      return {
-        id: notification.noti_id,
-        description: notification.noti_description,
-        title: notification.noti_title,
-        link: notification.noti_link
-      }
-    });
-    
-  client.query("SELECT tag_id FROM tags WHERE tags.tag_name = $1",[lastWord],selectTag);
-  }
-
-  function selectTag(err, results) {
-    result.tag = results.rows[0];
-        
-        client.query("SELECT * FROM profile WHERE profile.profile_id = $1",
-      [req.session.user.id], displayData);    
-    
-  }
-  
-  function displayData(err, results) {
-     res.render('view/ViewTagged.html', {
-    sitename: 'View Tagged',
-    tag_name: lastWord,
-    tag_id: result.tag,
-    project_id: result.project.p_id,
-    project_name: result.project.p_name,
-    group_id: result.group.g_id,
-    group_name: result.group.g_name,
-    event_id: result.event.e_id,
-    event_name: result.event.e_name,
-    profile_name: results.rows[0].profile_name,
-    notification: result.notifications,
-    admin_name: req.session.user.name,
-    user_email: req.session.user.user_email
-  }); 
-  }
-});
 
 module.exports = router;
--- src/routes/viewtagged.js
+++ src/routes/viewtagged.js
@@ -2,11 +2,81 @@ var express = require('express');
 var router = express.Router();
 var client = require('../../db').getClient();
 
-/* GET About Us page. */
-router.get('/', function (req, res, next) {
-	res.render('view/ViewTagged.html', {
-		sitename: 'View Tagged',
+//Method to give ViewTagged the info of the tag we clicked
+router.get('/', function (req, res, next) { 
+  //var url = req.url;
+  //console.log(url)
+  var result = {}
+  var tag_id;
+  var lastWord = []
+  lastWord[0] = req.query.tagButton; //url.split("/viewTagged?tagButton=").join('');
+  console.log(lastWord);
+
+  client.query("SELECT tag_id FROM tags WHERE tags.tag_name = $1", [lastWord[0]], selectTag);
+
+  function selectTag(err, results) {
+    tag_id= results.rows[0].tag_id;
+
+    client.query("SELECT * FROM project WHERE project.tag_id @> '{"+ tag_id +"}'::int[]",selectProjects);
+  }
+  function selectProjects(err, results) {
+    result.projects = results.rows.map(function (project) {
+      return {
+        p_id: project.project_id,
+        p_name: project.project_name
+      }
     });
+    client.query("SELECT * FROM groups WHERE groups.tag_id @> '{"+ tag_id +"}'::int[]",selectGroups);
+  }
+  function selectGroups(err, results) {
+    result.group = results.rows.map(function (group) {
+      return {
+        g_id: group.group_id,
+        g_name: group.group_name
+      }
+    });
+    client.query("SELECT * FROM event WHERE event.tag_list @> '{"+ tag_id +"}'::int[]",selectEvent);
+  }
+  function selectEvent(err, results) {
+    result.event = results.rows.map(function (event) {
+      return {
+        e_id: event.event_id,
+        e_name: event.event_name
+      }
+    });
+    client.query("SELECT * FROM notifications WHERE notifications.user_id = $1",
+      [req.session.user.id],
+      selectNotifications);
+  }
+
+  function selectNotifications(err, results) {
+    result.notifications = results.rows.map(function (notification) {
+      return {
+        id: notification.noti_id,
+        description: notification.noti_description,
+        title: notification.noti_title,
+        link: notification.noti_link
+      }
+    });
+
+    client.query("SELECT * FROM profile WHERE profile.profile_id = $1",
+      [req.session.user.id], displayData);
+  }
+
+  function displayData(err, results) {
+    res.render('view/ViewTagged.html', {
+      pagename: 'PIPS - View All Tags',
+      tag_name: lastWord[0],
+      tag_id: tag_id,
+      project: result.project,
+      group: result.group,
+      event: result.event,
+      profile_name: results.rows[0].profile_name,
+      notification: result.notifications,
+      admin_name: req.session.user.name,
+      user_email: req.session.user.user_email
+    });
+  }
 });
 
 module.exports = router;
